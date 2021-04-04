774e626181994f4ae94e1a0da89697bf97ea0534
Syed Muhammad Abdullah Zaydi <76549765+AbdullahZaydi@users.noreply.github.com>
2021-04-04 16:49:38 +0500

    New Changes

--- app/src/main/java/com/zayditech/helmetapp/LoginActivity.java
+++ app/src/main/java/com/zayditech/helmetapp/LoginActivity.java
@@ -2,7 +2,10 @@ package com.zayditech.helmetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +14,13 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         this.getSupportActionBar().hide();
+        TextView btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(v -> {
+            startActivity(new Intent(this, SignUpActivity.class));
+        });
+        ImageView btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(v -> {
+            super.onBackPressed();
+        });
     }
 }
--- app/src/main/java/com/zayditech/helmetapp/SignUpActivity.java
+++ app/src/main/java/com/zayditech/helmetapp/SignUpActivity.java
@@ -2,7 +2,10 @@ package com.zayditech.helmetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        this.getSupportActionBar().hide();
+        TextView btnLogin = findViewById(R.id.btnSignIn);
+        btnLogin.setOnClickListener(v -> {
+            startActivity(new Intent(this, LoginActivity.class));
+        });
+        ImageView btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(v -> {
+            super.onBackPressed();
+        });
     }
 }
